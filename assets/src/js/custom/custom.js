28011c35da93ae1f937b4d01fe4453ea716fc408
Ashok Lama <crazyhat17@gmail.com>
2025-03-21 10:47:08 +0545

    Removed icon fonts
    - Removed icon fonts

--- assets/src/js/custom/custom.js
+++ assets/src/js/custom/custom.js
@@ -33,7 +33,7 @@
 
         if (cream_magazine_script_obj.show_search_icon == '1') {
 
-            jQuery(".primary-navigation > ul").append('<li class="primarynav_search_icon"><a class="search_box" href="javascript:;"><i class="fa fa-search" aria-hidden="true"></i></a></li>');
+            jQuery(".primary-navigation > ul").append('<li class="primarynav_search_icon"><a class="search_box" href="javascript:;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></a></li>');
 
             /* Toggle header search container on click of search icon */
 
@@ -105,7 +105,7 @@
                 touchDrag: false,
                 animateOut: 'slideOutUp',
                 animateIn: 'slideInUp',
-                navText: ["<i class='feather icon-chevron-down'></i>", "<i class='feather icon-chevron-up'></i>"],
+                navText: ['<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>'],
             });
         }
 
@@ -123,7 +123,7 @@
                 autoplay: true,
                 autoplayTimeout: 3000,
                 autoplayHoverPause: true,
-                navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+                navText: ['<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
             });
         }
 
@@ -139,7 +139,7 @@
             autoplay: true,
             autoplayTimeout: 8000,
             autoplayHoverPause: true,
-            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            navText: ['<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
             responsive: {
                 0: {
                     items: 1
--- inc/theme-hooks.php
+++ inc/theme-hooks.php
@@ -254,7 +254,7 @@ if ( ! function_exists( 'cream_magazine_social_links_action' ) ) {
 							<?php
 						}
 						?>
-					><?php echo esc_html__( 'Facebook', 'cream-magazine' ); ?></a></li>
+					><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg><?php echo esc_html__( 'Facebook', 'cream-magazine' ); ?></a></li>
 				<?php
 			}
 
@@ -271,7 +271,7 @@ if ( ! function_exists( 'cream_magazine_social_links_action' ) ) {
 							<?php
 						}
 						?>
-					><?php echo esc_html__( 'Twitter', 'cream-magazine' ); ?></a></li>
+					><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg><?php echo esc_html__( 'Twitter', 'cream-magazine' ); ?></a></li>
 				<?php
 			}
 
@@ -288,7 +288,7 @@ if ( ! function_exists( 'cream_magazine_social_links_action' ) ) {
 							<?php
 						}
 						?>
-					><?php echo esc_html__( 'Instagram', 'cream-magazine' ); ?></a></li>
+					><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg><?php echo esc_html__( 'Instagram', 'cream-magazine' ); ?></a></li>
 				<?php
 			}
 
@@ -305,7 +305,7 @@ if ( ! function_exists( 'cream_magazine_social_links_action' ) ) {
 							<?php
 						}
 						?>
-					><?php echo esc_html__( 'Youtube', 'cream-magazine' ); ?></a></li>
+					><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg><?php echo esc_html__( 'YouTube', 'cream-magazine' ); ?></a></li>
 				<?php
 			}
 
@@ -322,7 +322,7 @@ if ( ! function_exists( 'cream_magazine_social_links_action' ) ) {
 							<?php
 						}
 						?>
-					><?php echo esc_html__( 'VK', 'cream-magazine' ); ?></a></li>
+					><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M31.5 63.5C0 95 0 145.7 0 247V265C0 366.3 0 417 31.5 448.5C63 480 113.7 480 215 480H233C334.3 480 385 480 416.5 448.5C448 417 448 366.3 448 265V247C448 145.7 448 95 416.5 63.5C385 32 334.3 32 233 32H215C113.7 32 63 32 31.5 63.5zM75.6 168.3H126.7C128.4 253.8 166.1 290 196 297.4V168.3H244.2V242C273.7 238.8 304.6 205.2 315.1 168.3H363.3C359.3 187.4 351.5 205.6 340.2 221.6C328.9 237.6 314.5 251.1 297.7 261.2C316.4 270.5 332.9 283.6 346.1 299.8C359.4 315.9 369 334.6 374.5 354.7H321.4C316.6 337.3 306.6 321.6 292.9 309.8C279.1 297.9 262.2 290.4 244.2 288.1V354.7H238.4C136.3 354.7 78 284.7 75.6 168.3z"/></svg><?php echo esc_html__( 'VK', 'cream-magazine' ); ?></a></li>
 				<?php
 			}
 
@@ -339,7 +339,7 @@ if ( ! function_exists( 'cream_magazine_social_links_action' ) ) {
 							<?php
 						}
 						?>
-					><?php echo esc_html__( 'Linkedin', 'cream-magazine' ); ?></a></li>
+					><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg><?php echo esc_html__( 'LinkedIn', 'cream-magazine' ); ?></a></li>
 				<?php
 			}
 
@@ -356,7 +356,7 @@ if ( ! function_exists( 'cream_magazine_social_links_action' ) ) {
 							<?php
 						}
 						?>
-					><?php echo esc_html__( 'Vimeo', 'cream-magazine' ); ?></a></li>
+					><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M447.8 153.6c-2 43.6-32.4 103.3-91.4 179.1-60.9 79.2-112.4 118.8-154.6 118.8-26.1 0-48.2-24.1-66.3-72.3C100.3 250 85.3 174.3 56.2 174.3c-3.4 0-15.1 7.1-35.2 21.1L0 168.2c51.6-45.3 100.9-95.7 131.8-98.5 34.9-3.4 56.3 20.5 64.4 71.5 28.7 181.5 41.4 208.9 93.6 126.7 18.7-29.6 28.8-52.1 30.2-67.6 4.8-45.9-35.8-42.8-63.3-31 22-72.1 64.1-107.1 126.2-105.1 45.8 1.2 67.5 31.1 64.9 89.4z"/></svg><?php echo esc_html__( 'Vimeo', 'cream-magazine' ); ?></a></li>
 				<?php
 			}
 			?>
@@ -408,7 +408,7 @@ if ( ! function_exists( 'cream_magazine_ticker_news_action' ) ) {
 			<div class="news_ticker_wrap clearfix">
 				<?php if ( ! empty( $news_ticker_section_title ) ) { ?>
 					<div class="ticker_head">
-						<span class="ticker_icon"><i class="fa fa-bolt" aria-hidden="true"></i></span>
+						<span class="ticker_icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="14" width="14"><path d="M349.4 44.6c5.9-13.7 1.5-29.7-10.6-38.5s-28.6-8-39.9 1.8l-256 224c-10 8.8-13.6 22.9-8.9 35.3S50.7 288 64 288l111.5 0L98.6 467.4c-5.9 13.7-1.5 29.7 10.6 38.5s28.6 8 39.9-1.8l256-224c10-8.8 13.6-22.9 8.9-35.3s-16.6-20.7-30-20.7l-111.5 0L349.4 44.6z"/></svg></span>
 						<div class="ticker_title"><?php echo esc_html( $news_ticker_section_title ); ?></div>
 					</div><!-- .ticker_head -->
 				<?php } ?>
@@ -911,7 +911,7 @@ if ( ! function_exists( 'cream_magazine_scroll_top_button_template' ) ) {
 			?>
 			<div class="backtoptop">
 				<button id="toTop" class="btn btn-info">
-					<i class="fa fa-angle-up" aria-hidden="true"></i>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="14" width="14"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>
 				</button>
 			</div><!-- ./ backtoptop -->
 			<?php
